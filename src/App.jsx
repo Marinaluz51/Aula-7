81f4c8cff964284c504eacd0ac9bbc86e0dd0c2d
Marina Luz <marina.luz@hotmail.com>
2024-02-27 22:51:16 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css';
+import List from './components/List';
+
+function App() {
+  const users = [
+    {
+      id: 0,
+      name: 'Siri'
+    },
+    {
+      id: 1,
+      name: 'Alexa'
+    },
+    {
+      id: 2,
+      name: 'Google Assistente'
+    },
+    {
+      id: 3,
+      name: 'Bixby'
+    }
+  ];
+
+
+  return (
+    <div>
+      <List items={users} />
+    </div>
+  );
+}
+
+export default App;
